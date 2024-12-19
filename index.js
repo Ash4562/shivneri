1e99207d061fba54939d026af552d5de41c16974
Your Name <you@example.com>
2024-12-19 20:33:46 +0530

    Your commit age

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ app.use("/api/booking", require("./routes/booking.routes")); // Route for genera
 
 // Catch-all for unmatched routes
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resource Not Found" });
+
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
 });
 
 // Global error handler
