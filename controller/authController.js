34e283bc860d1d51bd6708b1c045b60525ea154e
Your Name <you@example.com>
2025-01-11 01:13:19 +0530

    Your t message

--- controller/authController.js
+++ controller/authController.js
@@ -133,8 +133,8 @@ exports.sendResetLink = asyncHandler(async (req, res) => {
     }
 
     // Generate reset link
-    const resetLink = `https://shivneri.onrender.com/change-pass/${user._id}`;
-    // const resetLink = `http://localhost:5173/change-pass/${user._id}`;
+    // const resetLink = `https://shivneri.onrender.com/change-pass/${user._id}`;
+    const resetLink = `http://localhost:5173/change-pass/${user._id}`;
 
     // Send reset email
     await sendEmail({
